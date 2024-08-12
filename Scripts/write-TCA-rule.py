cfc6b7938de9347993cb7024322ce46139871069
Joshua Holden <holden@rose-hulman.edu>
2024-08-12 19:55:17 -0400

    Update write-TCA-rule.py

--- Scripts/write-TCA-rule.py
+++ Scripts/write-TCA-rule.py
@@ -11,6 +11,28 @@
 
 import golly as g
 
+def statetocrosstrin (state):
+    if len(state) < 2:
+        return(1) #noncrossing
+    elif state[1] = "L":
+        return(0) #left on top
+    elif state[1] = "R":
+        return(2) #right on top
+    else raise Exception("State not yet implemented")
+    
+def reflectstrand (strand):
+    if strand == "R":
+        return("L")
+    elif strand == "L":
+        return("R")
+    else:
+        return(strand)
+        
+        
+def reflectstate (state):
+    return(tuple(reversed(tuple(map(reflectstrand, state)))))
+    
+
 def setupruletable (crossingrule, turningrule):
     Rules = dict()
     bincrossrule = bin(crossingrule + 2^27)[-1:2:-1] #convert to binary in reverse
@@ -22,67 +44,62 @@ def setupruletable (crossingrule, turningrule):
     for i in states:
         for j in states:
             for k in states:
-                if not(("R" in i[0]) or ("C" in j[0]) or ("L" in k[0])):
-                    Rules[i, j, k] = () #nothing pointing into the cell
-                elif ("R" in i[0]) and ("C" not in j[0]) and ("L" not in k[0]):
-                    Rules[i, j, k] = ("R",) #only right pointing into the cell
-                elif ("R" not in i[0]) and ("C" not in j[0]) and ("L" in k[0]):
-                    Rules[i, j, k] = ("L",) #only left pointing into the cell  
-                elif ("R" not in i[0]) and ("C" in j[0]) and ("L" not in k[0]):
-                    Rules[i, j, k] = ("C",) #only center pointing into the cell  
-                else: #multiple pointing into the cell, deal with crossing first
-                    itrin = [("L","R"),("R",),("R","L")].index(i[0])
+                Rules[i,j,k] = ()
+                #put in strands
+                if ("L" in k[0]):
+                    Rules[i,j,k] = Rules[i,j,k] + ("L",)
+                if ("C" in j[0]):
+                    Rules[i,j,k] = Rules[i,j,k] + ("C",)
+                if ("R" in i[0]):
+                    Rules[i,j,k] = Rules[i,j,k] + ("R",)
+                #deal with crossing
+                itrin = statetocrosstrin(i[0])
                         #set itrin to 0, 1, 2 depending on left
+                jtrin = statetocrosstrin(j[0])
                         #set jtrin to 0, 1, 2 depending on center
-                    ktrin = [("R","L"),("L",),("L","R")].index(k[0])
+                ktrin = statetocrosstrin(k[0])
                         #set ktrin to 0, 1, 2 depending on right
-                    if (i[1] == "u"):
-                        itrin = 1 #non-crosssing
-                    if (k[1] == "u"):
-                        ktrin = 1 #non-crossing
-                    ijktrin = (9 * itrin + 3 * jtrin + ktrin) #0 to 26
-                    if ijktrin < 0 or 27 <= ijktrin:
-                        raise Exception("Error parsing crossing status")
-                    if bincrossrule[iktrin] == "0": 
-                        Rules[i, j, k] = ("L","R")
-                    elif bincrossrule[iktrin] == "1":
-                        Rules[i, j, k] = ("R","L")
-                    else:
-                        raise Exception("Bad crossing rule")
+                if (i[1] == "u"):
+                    itrin = 1 #non-crosssing
+                if (j[1] == "u"):
+                    jtrin = 1 #non-crosssing
+                if (k[1] == "u"):
+                    ktrin = 1 #non-crossing
+                ijktrin = (9 * itrin + 3 * jtrin + ktrin) #0 to 26
+                if ijktrin < 0 or 27 <= ijktrin:
+                    raise Exception("Error parsing crossing status")
+                if bincrossrule[ijktrin] == "0": 
+                    #nothing needs to be done
+                elif bincrossrule[ijktrin] == "1":
+                    Rules[i,j,k] = tuple(reversed(Rules[i,j,k])) 
+                        #this will need to be changed if more types of crossings allowed
+                        #possibly will need to set ijktrin before putting in strands
+                else:
+                    raise Exception("Bad crossing rule")
                 #start dealing with turning
                 if Rules[i, j, k] == ():
                     Rules[i, j, k] = (Rules[i, j, k], "e")
                 else: 
                     itrin = ["u","e","d"].index(i[1])
+                    jtrin = ["u","e","d"].index(j[1])
                     ktrin = ["u","e","d"].index(k[1])
                     if (i[0] == ()):
                         itrin = 1 #empty
+                    if (j[0] == ()):
+                        jtrin = 1 #empty
                     if (k[0] == ()):
                         ktrin = 1 #empty
-                    iktrin = 3 * itrin + ktrin #0 to 8
-                    if iktrin < 0 or 8 < iktrin:
-                        raise
-                    if binturnrule[iktrin] == "0":
-                        if Rules[i, j, k] == ("R",):
-                            Rules[i, j, k] = (("L",),"u")
-                        elif Rules[i, j, k] == ("L",):
-                            Rules[i, j, k] = (("R",),"u")
-                        else:
-                            Rules[i, j, k] = (Rules[i, j, k],"u")
+                    ijktrin = (9 * itrin + 3 * jtrin + ktrin) #0 to 26
+                    if ijktrin < 0 or 27 <= ijktrin:
+                        raise Exception("Error parsing turning status")
+                    if binturnrule[ijktrin] == "0":
+                        Rules[i,j,k] = reflectstate(Rules[i,j,k],"u")
                     elif binturnrule[iktrin] == "1":
                         Rules[i, j, k] = (Rules[i, j, k],"d")
                     else:
                         raise Exception("Bad turning rule")
     return(Rules)
 
-def statetocrosstrin (state):
-    if len(state) < 2:
-        return(1) #noncrossing
-    elif state[1] = "L"
-        return(0) #left on top
-    elif state[1] = "R"
-        return(2) #right on top
-    else raise Exception("State not yet implemented")
 
 def setupcelltodigittable ():
     celltodigit = dict()
@@ -103,7 +120,7 @@ def setupcelltodigittable ():
     celltodigit[(("C","L"),"d")] = 13 #13 center over left slanted (I over S) 
     celltodigit[(("C","R"),"d")] = 14 #14 center over right slanted (I over Z)
     celltodigit[(("L","C","R"),"u")] = 15 #15 left, center, right, upright
-    celltodigit[(("L","C","R"),"u")] = 15 #15 left, center, right, upright JUST IN CASE! 
+    celltodigit[(("R","C","L"),"u")] = 15 #15 left, center, right, upright JUST IN CASE! 
     celltodigit[(("L","C"),"d")] = 16 #16 left over center slanted (S over I)
     celltodigit[(("R","C"),"d")] = 17 #17 right over center slanted (Z over I)
     celltodigit[(("L","C","R"),"d")] = 18 #18 left, center, right slanted (S over I over Z)
